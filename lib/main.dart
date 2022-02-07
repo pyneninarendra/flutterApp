a27c4895b5b6ddd43678f8800aa7e409880bf70d
pyneninarendra <pyneninarendra@gmail.com>
2022-02-07 07:21:35 +0530

    container & text

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(title: "Awesome Title", home: HomePage()));
+  runApp(const MaterialApp(
+    title: "Awesome App",
+    home: HomeApp(),
+  ));
 }
 
-class HomePage extends StatelessWidget {
+class HomeApp extends StatelessWidget {
+  const HomeApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Awesome App")),
-      body: Container(
-        child: Center(
-          child: Container(
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5)],
-                color: Colors.red,
-                gradient: LinearGradient(colors: [Colors.teal, Colors.blue])),
-            width: 100,
-            height: 100,
-            child: Text("Box Demo"),
-            padding: const EdgeInsets.all(8),
+      appBar: AppBar(
+        title: const Text("Awesome App"),
+      ),
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          child: const Text(
+            "Hi Text",
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.teal,
+            boxShadow: const [
+              BoxShadow(
+                  color: Colors.grey, blurRadius: 5, offset: Offset(2.0, 5.0))
+            ],
+            gradient: const LinearGradient(colors: [Colors.red, Colors.yellow]),
           ),
         ),
       ),
