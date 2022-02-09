d02ef12a5974d0dc9f2ddb66fc4901a113d2feb9
pyneninarendra <pyneninarendra@gmail.com>
2022-02-09 07:32:05 +0530

    Chapter 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,42 +2,46 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    title: "Awesome App",
-    home: HomeApp(),
+    title: "First App",
+    home: FirstApp(),
   ));
 }
 
-class HomeApp extends StatelessWidget {
-  const HomeApp({Key? key}) : super(key: key);
+class FirstApp extends StatelessWidget {
+  const FirstApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Awesome App"),
+        title: const Text("First App"),
       ),
-      body: Center(
-        child: Container(
-          width: 100,
-          height: 100,
-          child: const Text(
-            "Hi Text",
-            textAlign: TextAlign.start,
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.teal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.green,
+              width: 100,
+              height: 100,
+              child: const Text("My First App"),
             ),
-          ),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Colors.teal,
-            boxShadow: const [
-              BoxShadow(
-                  color: Colors.grey, blurRadius: 5, offset: Offset(2.0, 5.0))
-            ],
-            gradient: const LinearGradient(colors: [Colors.red, Colors.yellow]),
-          ),
+            Container(
+              color: Colors.orange,
+              width: 100,
+              height: 100,
+              child: const Text("My First App"),
+            ),
+            Container(
+              color: Colors.red,
+              width: 100,
+              height: 100,
+              child: const Text("My First App"),
+            ),
+          ],
         ),
       ),
     );
